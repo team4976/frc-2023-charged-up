c40f196e74808dbb04c063eb96a578bf8f067687
ATM-TheChild <98564548+ATM-TheChild@users.noreply.github.com>
2023-01-17 20:29:22 -0500

    First push

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+/**
+ * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with tank
+ * steering and an Xbox controller.
+ */
+public class Robot extends TimedRobot {
+  private final TalonSRX m_leftMotor = new TalonSRX(43);
+  private final TalonSRX m_rightMotor = new TalonSRX(46);
+  private final XboxController m_driverController = new XboxController(0);
+
+  @Override
+  public void robotInit() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_rightMotor.setInverted(true);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Drive with tank drive.
+    // That means that the Y axis of the left stick moves the left side
+    // of the robot forward and backward, and the Y axis of the right stick
+    // moves the right side of the robot forward and backward.
+    m_leftMotor.set(ControlMode.PercentOutput, m_driverController.getLeftY());
+    m_rightMotor.set(ControlMode.PercentOutput, m_driverController.getRightY());
+  }
+}
