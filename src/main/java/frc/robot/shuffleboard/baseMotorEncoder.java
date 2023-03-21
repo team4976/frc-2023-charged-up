56d831521073d7da4926d60a7760a29f7fc315f1
Christian Verghis <89225559+ChristianVerghis@users.noreply.github.com>
2023-03-21 18:41:48 -0400

    Finished_Renaming_Files_Functional_Robot

---
+++ src/main/java/frc/robot/shuffleboard/baseMotorEncoder.java
@@ -0,0 +1,78 @@
+package frc.robot.shuffleboard;
+
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.robotConstants;
+import frc.robot.robotcode.commands.resetEncoder;
+
+public class baseMotorEncoder {
+
+    String name;
+    public BaseMotorController controller;
+    // private boolean type;
+    // ShuffleboardTab tab = shuffleBoard.instance.addTabs("Encoder");
+
+    public baseMotorEncoder(String name, VictorSPX victorSPX) {
+        this.name = name;
+        controller = victorSPX;
+        initShuffle();
+    }
+
+    public baseMotorEncoder(String name, TalonSRX talonSRX) {
+        this.name = name;
+        controller = talonSRX;
+        initShuffle();
+    }
+
+    public baseMotorEncoder(String name, TalonFX talonfx) {
+        this.name = name;
+        controller = talonfx;
+        initShuffle();
+    }
+
+    public baseMotorEncoder(){
+        ResetEncoder();
+    }
+
+    public void initShuffle() {
+        ShuffleboardTab tab = Shuffleboard.getTab("Encoder");
+
+        if (name.contains("_Encoder")) {
+            tab.addDouble(name + "_Distance", this::getDistance);
+            tab.addDouble(name + "_Speed", this::getRate);
+            tab.addDouble(name + "_Voltage", this::getVoltage);
+        } else {
+            tab.addDouble(name + "_Voltage", this::getVoltage);
+        }
+
+    }
+
+    //Test 
+    public void ResetEncoder(){
+        SmartDashboard.putData("Reast Encoder", new resetEncoder());
+    }
+
+    public double getVoltage() {
+        // ((VictorSPX)controller).getSelectedSensorPosition();
+        return controller.getBusVoltage();
+    }
+
+    public double getRate() {
+        return controller.getSelectedSensorVelocity();
+    }
+
+    public double getDistance() {
+        return controller.getSelectedSensorPosition();// test
+    }
+
+    // public double getCurrent(){
+    //     return controller.getStatorCurrent();
+    // }
+
+}
